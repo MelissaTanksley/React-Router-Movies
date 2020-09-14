c1bbd68d7e76b7010228c5bc3208f0634f809341
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-14 14:57:10 -0700

    movielist

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
+import MovieCard from './MovieCard';
 
-export default function MovieList(props) {
+const MovieList = props => {
+  const [movies] = useState([])
+ // console.error('Server Error', error);    
+ 
+
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
+      {movies.map(movie => (
+        <Link to={`/movies/${movie.id}`}>
+        <MovieCard key={movie.id} movie={movie} />
+      </Link>
+    ))}
     </div>
   );
 }
 
-function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
-  );
-}
+export default MovieList;
